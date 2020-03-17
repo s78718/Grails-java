cb31b503b500407eb0363b487a42dd3b324fa742
s78718 <s78718@gmail.com>
2020-03-18 03:56:26 +0800

    first

--- grails-app/controllers/ex/HelloController.groovy
+++ grails-app/controllers/ex/HelloController.groovy
@@ -3,6 +3,7 @@ package ex
 class HelloController {
 
     def index() { 
-        render "Hello World!"
+        render "today is new java framework test day in grails"
+        render  "create-controller hello"
     }
 }
