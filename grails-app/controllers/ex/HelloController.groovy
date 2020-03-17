7649afb359cb97416511ee8c351fc28e0da87a84
s78718 <s78718@gmail.com>
2020-03-18 03:28:10 +0800

    first

---
+++ grails-app/controllers/ex/HelloController.groovy
@@ -0,0 +1,8 @@
+package ex
+
+class HelloController {
+
+    def index() { 
+        render "Hello World!"
+    }
+}
---
+++ grails-app/init/ex/BootStrap.groovy
@@ -0,0 +1,9 @@
+package ex
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
